12fce723e543b8e03c359e700e7fa3ddc8434887
MR.RAMBABU <kumarrambabu0167@gmail.com>
2024-11-15 22:12:40 +0530

    Add files via upload

---
+++ addition.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class addition {
+    public static void main(String[] args) {
+       try (Scanner sc = new Scanner (System.in)) {
+        System.out.println("Enter your first number=");
+           int a =sc.nextInt();
+           int b =sc.nextInt();
+           int sum=a+b;
+           
+           System.out.print("SUM =\t"+sum);
+           
+         }
+      }
+   }
